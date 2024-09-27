4b06655b54675b287e7724654aa86b7e0f801400
WenXuan Lee <wenxuanlee@WenXuandeMacBook-Air.local>
2024-09-27 21:28:14 +0100

    UI: add sidebar and mobileNav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,36 @@
+import Image from 'next/image';
+import Sidebar from "@/components/Sidebar";
+import MobileNav from '@/components/MobileNav';
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={{
+        firstName: 'Lee',
+        lastName: 'Ben',
+      }}/>
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image
+            src="/icons/logo.svg"
+            alt="menu-icon"
+            width={30}
+            height={30}
+          />
+          <div>
+            <MobileNav user={{
+              firstName: 'Lee',
+              lastName: 'Ben',
+            }} />
+          </div>
+        </div>
+        {children}
+      </div>
+
     </main>
   );
 }
