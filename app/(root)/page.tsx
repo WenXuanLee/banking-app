7d704338f8bca180bead3658fbe5ad0ce4c43c0e
WenXuan Lee <wenxuanlee@WenXuandeMacBook-Air.local>
2024-09-27 22:11:39 +0100

    UI: Home right side and bank card layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import HeaderBox from '@/components/HeaderBox';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import RightSidebar from '@/components/RightSidebar';
 
 const Home = () => {
   return (
@@ -12,13 +13,20 @@ const Home = () => {
             title="Weclome"
             subtext="Access and manage your account"
             user="guest" />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250}
+          />
         </header>
-        <TotalBalanceBox
-          accounts={[]}
-          totalBanks={1}
-          totalCurrentBalance={1250}
-        />
+        RECENT TRANSCTION
       </div>
+      <RightSidebar
+        user={{ firstName: 'Ben', lastName: "Lee", email: 'test@gamil.com' }}
+        transactions={[]}
+        banks={[{ currentBalance: 1250 }, { currentBalance: 1550 }]}
+      />
     </section>
   )
 }
