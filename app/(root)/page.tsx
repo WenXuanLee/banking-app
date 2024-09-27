ef31e67cf55692f8ce7626435365d1f850a3ca63
WenXuan Lee <wenxuanlee@WenXuandeMacBook-Air.local>
2024-09-27 20:33:59 +0100

    UI: add Home greeting area

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,25 @@
+import React from 'react';
+import HeaderBox from '@/components/HeaderBox';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Weclome"
+            subtext="Access and manage your account"
+            user="guest" />
+        </header>
+        <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250}
+        />
+      </div>
+    </section>
   )
 }
 
