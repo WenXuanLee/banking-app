ccbe8f2104fca230c3400653e4b8cc8fd0287ca5
WenXuan Lee <wenxuanlee@WenXuandeMacBook-Air.local>
2024-09-29 20:50:28 +0100

    UI: Auth form layout

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,11 +1,11 @@
 import AuthForm from '@/components/AuthForm';
 
-const SignUp = () => {
+const SignIn = () => {
   return (
     <section className="flex-center size-full max-sm:px-6">
-      <AuthForm type="sign-up" />
+      <AuthForm type="sign-in" />
     </section>
   )
 }
 
-export default SignUp;
+export default SignIn;
