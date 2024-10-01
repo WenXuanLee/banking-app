89434576b39ca5218fdbf09bf20c639a96571257
WenXuan Lee <wenxuanlee@WenXuandeMacBook-Air.local>
2024-10-01 13:47:03 +0100

    UI: Transfer history page

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -20,7 +20,7 @@ const RecentTransactions = ({
       <header className="flex items-center justify-between">
         <h2 className="recent-transactions-label">Recent Transactions</h2>
         <Link
-          href={`/transaction-history/?id${appwriteItemId}`}
+          href={`/transaction-history/?id=${appwriteItemId}`}
           className="view-all-btn"
         >
           View All
